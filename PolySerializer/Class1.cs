a1cf280f7335f46baabb84d5a626aae0d595d5b1
dlebansais <dlebansais@gmail.com>
2017-12-02 17:12:43 +0100

    Add project files.

---
+++ PolySerializer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolySerializer
+{
+    public class Class1
+    {
+    }
+}
