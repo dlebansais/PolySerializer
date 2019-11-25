a8b5a825ba0be65ae44535f0cd9ca891b0718116
dlebansais <dlebansais@gmail.com>
2019-11-25 19:34:45 +0100

    Simplified some code.

--- PolySerializer/Serializer/Serialization/SerializationText.cs
+++ PolySerializer/Serializer/Serialization/SerializationText.cs
@@ -101,12 +101,42 @@
         {
             switch (value)
             {
+                case bool As_bool:
+                case char As_char:
+                case decimal As_decimal:
+                case double As_double:
+                case float As_float:
+                case string As_string:
+                case Guid As_Guid:
+                    return SerializeBasicType1_TEXT(value, ref data, ref offset);
                 case sbyte As_sbyte:
-                    AddFieldSByte_TEXT(ref data, ref offset, (sbyte)value);
-                    break;
                 case byte As_byte:
-                    AddFieldByte_TEXT(ref data, ref offset, (byte)value);
+                case int As_int:
+                case long As_long:
+                case short As_short:
+                case uint As_uint:
+                case ulong As_ulong:
+                case ushort As_ushort:
+                    return SerializeBasicType2_TEXT(value, ref data, ref offset);
+
+                default:
+                    Type ValueType = value.GetType();
+
+                    if (ValueType.IsEnum)
+                        SerializeEnumType_TEXT(value, ValueType, ref data, ref offset);
+                    else
+                        return false;
                     break;
+            }
+
+            return true;
+        }
+
+        private bool SerializeBasicType1_TEXT(object value, ref byte[] data, ref int offset)
+        {
+            switch (value)
+            {
+                default:
                 case bool As_bool:
                     AddFieldBool_TEXT(ref data, ref offset, (bool)value);
                     break;
@@ -121,6 +151,28 @@
                     break;
                 case float As_float:
                     AddFieldFloat_TEXT(ref data, ref offset, (float)value);
+                    break;
+                case string As_string:
+                    AddFieldString_TEXT(ref data, ref offset, (string)value);
+                    break;
+                case Guid As_Guid:
+                    AddFieldGuid_TEXT(ref data, ref offset, (Guid)value);
+                    break;
+            }
+
+            return true;
+        }
+
+        private bool SerializeBasicType2_TEXT(object value, ref byte[] data, ref int offset)
+        {
+            switch (value)
+            {
+                default:
+                case sbyte As_sbyte:
+                    AddFieldSByte_TEXT(ref data, ref offset, (sbyte)value);
+                    break;
+                case byte As_byte:
+                    AddFieldByte_TEXT(ref data, ref offset, (byte)value);
                     break;
                 case int As_int:
                     AddFieldInt_TEXT(ref data, ref offset, (int)value);
@@ -139,21 +191,6 @@
                     break;
                 case ushort As_ushort:
                     AddFieldUShort_TEXT(ref data, ref offset, (ushort)value);
-                    break;
-                case string As_string:
-                    AddFieldString_TEXT(ref data, ref offset, (string)value);
-                    break;
-                case Guid As_Guid:
-                    AddFieldGuid_TEXT(ref data, ref offset, (Guid)value);
-                    break;
-
-                default:
-                    Type ValueType = value.GetType();
-
-                    if (ValueType.IsEnum)
-                        SerializeEnumType_TEXT(value, ValueType, ref data, ref offset);
-                    else
-                        return false;
                     break;
             }
 
