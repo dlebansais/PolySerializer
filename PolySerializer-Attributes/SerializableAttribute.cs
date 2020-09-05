11f0d6fb8521db138db45c7c288a54078c482aa2
dlebansais <dlebansais@gmail.com>
2020-09-05 12:09:37 +0200

    Test

--- PolySerializer-Attributes/SerializableAttribute.cs
+++ PolySerializer-Attributes/SerializableAttribute.cs
@@ -9,25 +9,25 @@
     public class SerializableAttribute : Attribute
     {
         /// <summary>
-        /// Get or set a flag indicating if the member should be excluded from serialization (and deserialization).
+        /// Gets or sets a value indicating whether the member should be excluded from serialization (and deserialization).
         /// </summary>
         public bool Exclude { get; set; }
 
         /// <summary>
-        /// Get or set a condition for deserializing a member.
+        /// Gets or sets a condition for deserializing a member.
         /// If the boolean member indicated by <see cref="Condition"/> is set to true, the member with this attribute is serialized, otherwise it is ignored and won't be deserialized.
         /// </summary>
         public string Condition { get; set; }
 
         /// <summary>
-        /// Get or set the name of a setter for deserializing a member.
+        /// Gets or sets the name of a setter for deserializing a member.
         /// Applies only to read-only properties.
         /// If the member indicated by <see cref="Setter"/> is a method taking only one argument of the same type, the method is called with the value to deserialize.
         /// </summary>
         public string Setter { get; set; }
 
         /// <summary>
-        /// Get or set the list of property values to use to construct a deserialized object.
+        /// Gets or sets the list of property values to use to construct a deserialized object.
         /// Applies only to one of the object's constructors.
         /// If this attribute isn't specified, the parameterless constructor is used.
         /// If specified, the constructor must have parameters, their name must match properties of the object's type, and this attribute must list them as a comma-separated list. Serialized values of these properties are used in the same order when calling the constructor.
